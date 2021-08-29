2ed7a1269d785b74b9465d969c8cccbc310df382
jswap21 <74594485+jswap21@users.noreply.github.com>
2021-08-29 18:14:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,149 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+
+  var PLAY = 1;
+var END = 0;
+var gameState = "start";
+
+var balls=[];
+var ball;
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var divisionHeight=300;
+var score =0;
+var particle;
+var count=0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ text("Score : "+score,20,30);
+ textSize(40)
+ text("500",165,650);
+ text("500",250,650);
+ text("500",330,650);
+ text("500",85,650);
+ text("500",5,650);
+ text("100",400,650);
+ text("100",480,650);
+ text("100",560,650);
+ text("100",640,650);
+ text("100",720,650);
+
+  Engine.update(engine);
+ // if(gameState===PLAY){
+
+  
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+ //if(frameCount%60===0){
+ //particles.push(new Particle(random(width/2-10,width/2+10,),10,10));
+ //score=score++
+ //}
+ 
+  for (var j = 0; j < particles.length; j++) {
+  
+   particles[j].display();
+ }
+
+  
+  
+
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+ 
+ // }
+
+ if(ball!=null)
+    {
+       ball.display();
+        
+        if (ball.body.position.y>760)
+        {
+              if (ball.body.position.x < 300) 
+              {
+                  score=score+500;      
+                  ball=null;
+                  if ( count>= 5) gameState ="end";                          
+              }
+
+
+              else if (ball.body.position.x < 600 && ball.body.position.x > 301 ) 
+              {
+                    score = score + 100;
+                    ball=null;
+                    if ( count>= 5) gameState ="end";
+
+              }
+              else if (ball.body.position.x < 900 && ball.body.position.x > 601 )
+              {
+                    score = score + 200;
+                    ball=null;
+                    if ( count>= 5)  gameState ="end";
+
+              }      
+              
+        }
+  
+      }
+}
+function mousePressed()
+{
+if(gameState!=="end"){
+count++;
+ball=new Ball(mouseX,10,10,10);
+}
+}
